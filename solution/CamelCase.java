c2d5d39a3b76f1dce174a7b198572e541c1614a8
Bastien Krettly <bastien@wildcodeschool.fr>
2017-12-11 13:18:52 +0100

    Update CamelCase.java

--- solution/CamelCase.java
+++ solution/CamelCase.java
@@ -1,4 +1,19 @@
 class CamelCase {
 
-	// TODO
+	public static String convert(String text) {
+		boolean capitalize = false;
+		String result = "";
+		for (int i = 0; i < text.length(); i++) {
+			char letter = text.charAt(i);
+			if (letter == ' ') {
+				capitalize = true;
+			} else if (capitalize) {
+				capitalize = false;
+				result += Character.toUpperCase(letter);
+			} else {
+				result += Character.toLowerCase(letter);
+			}
+		}
+		return result;
+	}
 }
