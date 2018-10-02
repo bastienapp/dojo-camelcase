1e8477c8659bfbad37b1c06c7f1bd14c8ab93b8b
Bastien Krettly <bastien_krettly@wilder.school>
2018-10-02 12:31:50 +0200

    fix solution

--- solution/CamelCase.java
+++ solution/CamelCase.java
@@ -1,19 +1,25 @@
 class CamelCase {
 
-	public static String convert(String text) {
-		boolean capitalize = false;
-		String result = "";
-		for (int i = 0; i < text.length(); i++) {
-			char letter = text.charAt(i);
-			if (letter == ' ') {
-				capitalize = true;
-			} else if (capitalize) {
-				capitalize = false;
-				result += Character.toUpperCase(letter);
-			} else {
-				result += Character.toLowerCase(letter);
-			}
-		}
-		return result;
+	public static String convert(String str){
+	    str = str.toLowerCase();
+	    String result = "";
+	    boolean isNextUpper = false; 
+	    
+	    for (int i = 0; i < str.length(); i ++) {
+	        char currentChar = str.charAt(i);
+	        if (currentChar != ' ') {
+	            
+	            if (isNextUpper == true) {
+	                // transforme le caractère à ajouter en majuscule
+	                currentChar = Character.toUpperCase(currentChar);
+	                isNextUpper = false;  
+	            }
+                result += currentChar;                   
+	        } else {
+	            // prochain caractère en majuscule
+	            isNextUpper = true; 
+	        }
+	    }
+	    return result;
 	}
 }
